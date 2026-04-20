be340aa8bbed836f39fe5c413662cb88450b849d
João M. Gabaldi <167992334+joaomgabaldi@users.noreply.github.com>
2026-04-20 13:13:36 -0300

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,32 +1,33 @@
 export const rdStorage = {
-  async get(keys) { 
-    return browser.storage.local.get(keys); 
+  get: (keys) => browser.storage.local.get(keys),
+  set: (data) => browser.storage.local.set(data),
+  remove: (keys) => browser.storage.local.remove(keys),
+  
+  saveCachedDownloads: (downloads) => browser.storage.local.set({ rd_cached_downloads: downloads }),
+  
+  getLocalDownloads: async () => {
+    const res = await browser.storage.local.get('rd_local_downloads');
+    return res.rd_local_downloads || [];
   },
-  async set(data) { 
-    return browser.storage.local.set(data); 
-  },
-  async remove(keys) { 
-    return browser.storage.local.remove(keys); 
-  },
-  async getCachedDownloads() {
-    const data = await this.get('rd_cached_downloads');
-    return data.rd_cached_downloads || [];
-  },
-  async saveCachedDownloads(downloads) {
-    return this.set({ rd_cached_downloads: downloads });
-  },
-  async getLocalDownloads() {
-    const data = await this.get('rd_local_downloads');
-    return data.rd_local_downloads || [];
-  },
-  async saveLocalDownloads(downloads) {
-    return this.set({ rd_local_downloads: downloads });
-  },
-  async getLocalNotifications() {
-    const data = await this.get('rd_local_notifications');
-    return data.rd_local_notifications || [];
-  },
-  async saveLocalNotifications(notifications) {
-    return this.set({ rd_local_notifications: notifications });
-  }
+  
+  saveLocalDownloads: (downloads) => browser.storage.local.set({ rd_local_downloads: downloads }),
+
+  saveOAuthPending: (data) => browser.storage.local.set({ rd_oauth_pending: data }),
+  removeOAuthPending: () => browser.storage.local.remove('rd_oauth_pending'),
+
+  saveAuthData: (data) => browser.storage.local.set({
+    rd_access_token: data.access_token,
+    rd_refresh_token: data.refresh_token,
+    rd_oauth_client_id: data.client_id,
+    rd_oauth_client_secret: data.client_secret,
+    rd_token_expires_at: data.expires_at
+  }),
+
+  clearAuthData: () => browser.storage.local.remove([
+    'rd_access_token', 'rd_refresh_token', 'rd_oauth_client_id', 
+    'rd_oauth_client_secret', 'rd_token_expires_at', 'rd_cached_user', 
+    'rd_cached_downloads', 'rd_oauth_pending', 'rd_ignore_locks',
+    'rd_tracked_ids', 'rd_local_notifications', 'rd_local_downloads',
+    'rd_use_vlc'
+  ])
 };
